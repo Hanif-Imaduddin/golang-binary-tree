c4c7eb2638fb0f676e9c93539c896bba3474bcf0
Hanif Imaduddin <aadepok128256@gmail.com>
2023-12-01 17:11:57 +0700

    Menyelesaikan binary tree

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"log"
+
 	// "log"
 	"strconv"
 	"strings"
@@ -59,15 +61,75 @@ func (b *bst) addByNode(root *node, value int) *node {
 	return root
 }
 
-func main() {
-	b := &bst{
-		root: nil,
-		len:  0,
+func (b bst) search(value int) (*node, bool) {
+	return b.searchByNode(b.root, value)
+}
+func (b bst) searchByNode(root *node, value int) (*node, bool) {
+	if root == nil {
+		return nil, false
 	}
-	b.add(65)
-	b.add(32)
-	b.add(17)
-	b.add(14)
-	fmt.Println(b)
-	fmt.Println(b.len)
+
+	if value == root.value {
+		return root, true
+	} else if value < root.value {
+		return b.searchByNode(root.left, value)
+	} else {
+		return b.searchByNode(root.right, value)
+	}
+}
+
+func (b *bst) remove(value int) {
+	b.root = b.removeByNode(b.root, value)
+}
+func (b *bst) removeByNode(root *node, value int) *node {
+	if root == nil {
+		return root
+	}
+	if value > root.value {
+		root.right = b.removeByNode(root.right, value)
+	} else if value < root.value {
+		root.left = b.removeByNode(root.left, value)
+	} else {
+		if root.left == nil {
+			return root.right
+		} else {
+			temp := root.left
+			for temp.right != nil {
+				temp = temp.right
+			}
+			root.value = temp.value
+			log.Println("Value temp", temp.value)
+			root.left = b.removeByNode(root.left, temp.value)
+		}
+	}
+	return root
+}
+
+func main() {
+	// n := &node{value: 1, left: nil, right: nil}
+	// n.left = &node{value: 0, left: nil, right: nil}
+	// n.right = &node{value: 2, left: nil, right: nil}
+
+	// b := &bst{
+	// 	root: n,
+	// 	len:  0,
+	// }
+	// b.add(5)
+	// b.add(4)
+	// b.add(6)
+	// fmt.Println(b)
+	// b.remove(0)
+	// fmt.Println(b)
+	bst := &bst{}
+	bst.add(5)
+	bst.add(3)
+	bst.add(7)
+	bst.add(2)
+	bst.add(4)
+	bst.add(6)
+	bst.add(8)
+
+	fmt.Println(bst)
+	bst.remove(3)
+	fmt.Println(bst)
 }
